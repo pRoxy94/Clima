eb3f32803c62a8383d37026b33391d061d0ef621
Rossana Laricchia <rossana.laricchia@eng.it>
2022-05-10 14:52:02 +0200

    Added UITextFieldDelegate and methods for manage editing and validation of text

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,48 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+/**
+ UItextFieldDelegate: manage the editing and validation of text
+ */
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+	   // the textfield should report back to our view controller
+	   // self refers to the current view controller
+	   searchTextField.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+	   // hide keyboard
+	   searchTextField.endEditing(true)
+	   print(searchTextField.text!)
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+	   // hide keyboard
+	   searchTextField.endEditing(true)
+	   print(searchTextField.text!)
+	   return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+	   searchTextField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+	   if textField.text != "" {
+		  return true
+	   } else {
+		  textField.placeholder = "Type something"
+		  return false
+	   }
+    }
 }
 
